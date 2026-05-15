46d950281ad1120796e5d0084c5ef80f245727f6
Denys Bokov <denisbokov1703@gmail.com>
2026-05-15 14:08:57 +0200

    feat: update static and media urls and connect them in urls.py

--- management/settings.py
+++ management/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 AUTH_USER_MODEL = 'user.User'
