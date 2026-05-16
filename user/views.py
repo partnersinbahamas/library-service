99bc960b468175d1b05d5ac87605cf2055a06891
Denys Bokov <denisbokov1703@gmail.com>
2026-05-16 14:47:58 +0200

    feat(user): add register, login, refresh and me views for users with serializer

--- user/views.py
+++ user/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+class CreateUserView(CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ManageUserView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
