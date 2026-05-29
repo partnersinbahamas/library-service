b2963be02f6cd36c45db10630890ea35242180db
Denys Bokov <denisbokov1703@gmail.com>
2026-05-29 17:49:34 +0200

    feat(service): create BookViewSet and register service routing

--- library/views.py
+++ library/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from rest_framework import viewsets
+
+from library.models import Book
+from library.serializers import BookSerializer, BookListSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookListSerializer
+
+    def get_serializer_class(self):
+        match self.action:
+            case "list":
+                return BookListSerializer
+            case _:
+                # BookListSerializer as a default serializer
+                return BookSerializer
