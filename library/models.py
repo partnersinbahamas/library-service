84e508ba0b0229e5a132c769314f3c96e07e4073
Denys Bokov <denisbokov1703@gmail.com>
2026-05-31 18:56:39 +0200

    feat(service): update fields, and add summaries book field

--- library/models.py
+++ library/models.py
@@ -6,9 +6,13 @@ from library.utils import get_current_year
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=100, db_index=True)
-    author = models.CharField(max_length=100)
-    cover = models.CharField(choices=BookCoverChoices.choices, max_length=10)
+    api_id = models.IntegerField(unique=True, default=None, null=True)
+    title = models.CharField(max_length=255, db_index=True)
+    author = models.CharField(max_length=255)
+    summaries = models.TextField(blank=True, null=True)
+    cover = models.CharField(
+        choices=BookCoverChoices.choices, default=BookCoverChoices.HARD, max_length=10
+    )
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
     year_of_publication = models.PositiveIntegerField(
