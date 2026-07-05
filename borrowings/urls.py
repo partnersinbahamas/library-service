22ba1bc4dc93067cd347e2b11821d13593bc27e2
Denys Bokov <denisbokov1703@gmail.com>
2026-07-05 19:37:40 +0200

    feat(borrowings): create borrowings views

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,1 +1,13 @@
-urlpatterns = []
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from borrowings.views import BorrowingViewSet
+
+app_name = "borrowings"
+
+router = DefaultRouter()
+router.register("borrowings", BorrowingViewSet, basename="borrowings")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
